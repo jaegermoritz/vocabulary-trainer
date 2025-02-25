7cc673a4e3af3fbb26af6e4023d26fab8c711d27
Moritz <moritz.jaeger@genius7.de>
2025-02-25 10:45:26 +0100

    fix(graphql-server): :wrench: fix path to test.db in jest setup and teardown

--- apps/graphql/jest.integration.teardown.ts
+++ apps/graphql/jest.integration.teardown.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { emojify } from "node-emoji";
 
-const testDbPath = path.resolve(__dirname, "test.db");
+const testDbPath = path.resolve(__dirname, "prisma", "test.db");
 
 module.exports = async () => {
   try {
@@ -15,7 +15,22 @@ module.exports = async () => {
       console.log(
         emojify(`:wastebasket: Deleting test database at ${testDbPath}`)
       );
-      fs.unlinkSync(testDbPath);
+      fs.unlink(testDbPath, (unlinkError) => {
+        if (unlinkError) {
+          console.error(
+            emojify(`:x: Failed to delete test database:`),
+            unlinkError
+          );
+        } else {
+          console.log(
+            emojify(`:white_check_mark: Test database deleted successfully`)
+          );
+        }
+      });
+    } else {
+      console.log(
+        emojify(`:warning: Test database not found at ${testDbPath}`)
+      );
     }
   } catch (error) {
     console.error(emojify(`):x: Error during Jest global teardown:`), error);
