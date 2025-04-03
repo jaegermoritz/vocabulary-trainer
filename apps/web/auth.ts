56c43e4b5e021e49706588297ceee13ebdb154ff
Moritz <66466031+jaegermoritz@users.noreply.github.com>
2025-04-03 13:26:07 +0200

    feat(web-app): :sparkles: add password authentication (#48)
    * add auth.js password authentication provider
    * setup playwright
    * add playwright tests for password authentication
    * add basic documentation for application testing

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -1,8 +1,37 @@
-import NextAuth, { NextAuthConfig, NextAuthResult } from 'next-auth'
+import NextAuth, { NextAuthConfig, NextAuthResult, User } from 'next-auth'
+import { Provider } from 'next-auth/providers'
+import Credentials from 'next-auth/providers/credentials'
 import GitHub from 'next-auth/providers/github'
 
+// Define the providers you want to use:
+const providers: Provider[] = [GitHub]
+
+// Use the password provider in development and test for AUTOMATIC TESTING ONLY!
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  providers.push(
+    Credentials({
+      id: 'password',
+      name: 'Password',
+      credentials: {
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        let user: User | null = null
+        if (credentials.password === process.env.TEST_PASSWORD) {
+          user = {
+            email: 'bob@alice.com',
+            name: 'Bob Alice',
+            image: 'https://avatars.githubusercontent.com/u/67470890?s=200&v=4',
+          }
+        }
+        return (await Promise.resolve(user)) ?? null
+      },
+    })
+  )
+}
+
 const options: NextAuthConfig = {
-  providers: [GitHub],
+  providers,
 }
 
 const nextAuth = NextAuth(options)
